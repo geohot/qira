3adea6b69c74fec7a0c79587d6a194f899f49ce1
George Hotz <geohot@google.com>
2014-06-26 15:04:59 -0700

    deleted cfg stuff

--- web/server/kadira.js
+++
@@ -1,2 +0,0 @@
-//Kadira.connect('9ycRJpQj8kMtGiAxq', '793dc5b8-8daa-4c9d-ad07-5a5c35eb9073');
-
--- web/server/qira.js
+++ web/server/qira.js
@@ -17,62 +17,6 @@ Meteor.publish('dat_clnum', function(clnum){
   return Change.find({clnum: clnum}, {sort: {address: 1}, limit:20});
 });
 
-Meteor.publish('instruction_iaddr', function(iaddr){
-  // this doesn't work...
-  //return Program.find({address: {$gt: iaddr-0x100, $lt: iaddr+0x100}}, {sort: {address:1}});
-});
-
-Meteor.publish('loops', function() {
-  return Loops.find();
-});
-
-Meteor.publish('fxns', function() {
-  var fxns = Fxns.find(); // bad
-  return fxns;
-});
-
-Meteor.publish('blocks', function() {
-  var fxns = Blocks.find(); // bad
-  return fxns;
-});
-
-Meteor.publish('instructions', function(clnum, collapsed) {
-  var start_time = process.hrtime();
-
-  var BEFORE = clnum-0x10;
-  var and = [{clend: {$gt: BEFORE}}];
-  for (var i = 0; i < collapsed.length; i++) {
-    and.push({$or: [{clstart: {$lt: collapsed[i][0]}}, {clend: {$gt: collapsed[i][1]}}]});
-  }
-
-  //return Change.find({clnum: {$gt: clnum-0x10, $lt: clnum+0x18}, type: "I"}, {sort: {clnum:1}});
-  var cblocks = Blocks.find({$and: and}, {limit: 20});
-  //cblocks.forEach(function(post) { console.log(post); });
-
-  // build the changelist fetching query from the blocks
-  var query = [];
-  cblocks.forEach(function(post) { 
-    // get one past the end of each visible block
-    query.push({clnum: {$gte: post.clstart, $lte: (post.clend+1)}})
-  });
-  if (query.length == 0) { console.log("cl query failed"); return; }
-
-  // limit here should be the onscreen blocks
-  var changes = Change.find({$or: query, type: "I"}, {sort: {clnum: 1}, limit: 0x50});
-
-  // build the address fetching query from the changelists
-  var query = [];
-  changes.forEach(function(post) { query.push({address: post.address}); });
-  if (query.length == 0) { console.log("ins query failed"); return; }
-  var progdat = Program.find({$or: query});
-
-  // this is taking 200ms
-  var end_time = process.hrtime(start_time);
-  console.log("instructions took "+end_time[0]*1000.0 + end_time[1]/1000000.0+" ms");
-
-  // we need to send the program data back here as well...
-  return [changes, cblocks, progdat];
-});
 
 Meteor.publish('dat_iaddr', function(iaddr){
   // fetch the static info about the range
@@ -87,9 +31,5 @@ Meteor.publish('dat_daddr', function(daddr){
   } else {
     return false;
   }
-});
-
-Meteor.publish('hexedit_daddr', function(daddr, clnum) {
-  return Change.find({address:daddr, clnum:{$lte: clnum}}, {sort: {clnum: -1}, limit:1});
 });
 
