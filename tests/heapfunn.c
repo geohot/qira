34d51258c61b8f4970cb055973bd304e10564d68
George Hotz <geohot@gmail.com>
2014-08-01 21:33:32 +0000

    heapfunn

--- tests/heapfunn.c
+++ tests/heapfunn.c
@@ -5,13 +5,15 @@
 char *bufs[NUMBUFS];
 
 int main() {
-  unsigned int bufnum, size, i;
+  unsigned int bufnum, size, haxx, i;
   for (i = 0; i < NUMBUFS; i++) bufs[i] = NULL;
   printf("exploit me bro\n");
+  fflush(stdout);
   while (1) {
     read(0, &bufnum, sizeof(bufnum));
     if (bufnum >= NUMBUFS) continue;
     read(0, &size, sizeof(size));
+    read(0, &haxx, sizeof(haxx));
     if (size == 0) {
       if (bufs[bufnum] != NULL) free(bufs[bufnum]);
       bufs[bufnum] = NULL;
@@ -19,8 +21,8 @@ int main() {
       bufs[bufnum] = (char *)malloc(size);
       if (bufs[bufnum] != NULL) {
         int r = read(0, bufs[bufnum], size);
-        if (r > 0) {
-          bufs[bufnum][r] = '\0';
+        if (haxx) {
+          bufs[bufnum][size] = '\0';
         }
       }
     }
