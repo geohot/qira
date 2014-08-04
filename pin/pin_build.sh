d657b70653fc560f46d788316f3478a9043873b6
George Hotz <geohot@gmail.com>
2014-08-04 19:19:41 +0000

    added build script to bring in line with the rest of QIRA

---
+++ pin/pin_build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+if [ ! -d pin-latest ]; then
+  wget -O- http://software.intel.com/sites/landingpage/pintool/downloads/pin-2.13-65163-gcc.4.4.7-linux.tar.gz | gunzip | tar x
+  ln -s pin-2.13-65163-gcc.4.4.7-linux pin-latest
+fi
+
+export PIN_ROOT=./pin-latest
+make
+
