87bc77eebbc534a741f43a723471d8db0154d079
George Hotz <geohot@google.com>
2014-08-20 00:43:51 -0700

    arg cleanup and bug fix

--- middleware/qira_config.py
+++ middleware/qira_config.py
@@ -6,7 +6,7 @@ TRACE_LIBRARIES = False
 WEB_HOST = '127.0.0.1'
 WEB_PORT = 3002
 SOCAT_PORT = 4000
-FORK_PORT = 4001
+FORK_PORT = SOCAT_PORT + 1
 USE_PIN = False
 if os.name == "nt":
   TRACE_FILE_BASE = "c:/qiratmp"
