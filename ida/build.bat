64617015298327370f18a761d9c7a288e5a0872d
George Hotz <geohot@google.com>
2014-08-21 10:34:52 -0700

    fixing the windows plugin build

--- ida/build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-IF "%SDKROOT%"=="" ( 
-	ECHO SDKROOT is not defined. Please set it to the root directory of the IDA SDK.
-	exit /b
-)
-
-PUSHD win32
-msbuild
-POPD
--- ida/mingw_build.sh
+++ ida/mingw_build.sh
@@ -1,17 +1,34 @@
 #!/bin/bash -e
 
+# build websockets with
+# cmake .. -DCMAKE_TOOLCHAIN_FILE=../cross-ming.cmake -DLWS_WITH_SSL=0
+
 SDKROOT=~/build/idasdk66
 OUTPUT="qira.plw"
 OUTPUT64="qira.p64"
 OPENSSL=~/.wine/drive_c/OpenSSL-Win32
+MINGW_PREFIX="i686-w64-mingw32"
 
-i586-mingw32msvc-g++ template.cpp -fPIC -D__IDP__ -D__PLUGIN__ -c -D__NT__ -I . -I$SDKROOT/include
+# build 32
+$MINGW_PREFIX-g++ template.cpp -fPIC -D__IDP__ -D__PLUGIN__ -c -D__NT__ -I . -I$SDKROOT/include
+$MINGW_PREFIX-g++ --shared template.o $SDKROOT/lib/x86_win_gcc_32/ida.a \
+  -static-libgcc -static-libstdc++ \
+  libs/libwebsockets_static.a \
+  libs/libZLIB.a \
+  -o $OUTPUT -lws2_32
+echo "built 32"
 
-i586-mingw32msvc-g++ --shared template.o $SDKROOT/lib/x86_win_gcc_32/ida.a -o $OUTPUT \
-  -lws2_32 \
-  $OPENSSL/lib/libeay32.lib \
-  $OPENSSL/lib/ssleay32.lib \
-  libs/websockets_static.lib \
-  libs/ZLIB.lib
+# build 64
+$MINGW_PREFIX-g++ template.cpp -D__EA64__=1 -fPIC -D__IDP__ -D__PLUGIN__ -c -D__NT__ -I . -I$SDKROOT/include
+$MINGW_PREFIX-g++ --shared template.o $SDKROOT/lib/x86_win_gcc_32/ida.a \
+  -static-libgcc -static-libstdc++ \
+  libs/libwebsockets_static.a \
+  libs/libZLIB.a \
+  -o $OUTPUT64 -lws2_32
+echo "built 64"
 
+sha1sum $OUTPUT $OUTPUT64
+
+cp $OUTPUT bin/qira_ida66_windows.plw
+cp $OUTPUT64 bin/qira_ida66_windows.p64
 
