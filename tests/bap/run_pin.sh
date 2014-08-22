4f3d4a3dae009cbc076ed697b6492b0b872c06bc
George Hotz <geohot@google.com>
2014-08-21 22:11:09 -0700

    add hop for mcsema

--- tests/bap/run_pin.sh
+++ tests/bap/run_pin.sh
@@ -1,4 +1,17 @@
-#!/bin/sh
-~/bap/pin/pin -t ~/bap/pintraces/obj-ia32/gentrace.so -logall_after -taint_args -- ~/qira/tests/changetest 23
-~/bap/utils/toil -serializedtrace out.bpt > out.useful
+#!/bin/bash -e
+
+# using https://github.com/vrtadmin/moflow/tree/master/BAP-0.7-moflow
+rm out.*
+
+# -logall_after
+~/bap/pin/pin -t ~/bap/pintraces/obj-ia32/gentrace.so -taint_args -- ~/qira/tests/changetest 23
+~/bap/utils/toil -serializedtrace out.bpt -o out.il
+~/bap/utils/ileval -eval -il out.il
+exit
+
+#~/bap/custom_utils/prep-slice.sh out.bpt out.il
+~/bap/utils/iltrans -serializedtrace out.bpt -trace-concrete-subst -trace-dsa -pp-ast out.ssail
+
+#~/bap/custom_utils/slicer -il out.il -var dsa_R_DFLAG_1_37201 -b -o out.slice
+~/bap/custom_utils/slicer -il out.ssail -var dsa_R_EAX_1_9044 -f -o out.slice
 
