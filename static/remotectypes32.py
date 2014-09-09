4f35dbbf4ca6b9e31502a2e5d8033a2a678d6606
Ryan Goulden <percontation@gmail.com>
2014-09-09 03:21:44 -0400

    old remoteobjects 0.1 update

--- static/remotectypes32.py
+++ static/remotectypes32.py
@@ -1,25 +1,37 @@
+# TODO: Switch to an stdio based method instead of needing to TCP server.
 import remoteobj
-
-ADDR = ('localhost', 43673)
-PYTHON32 = ''
 
 if __name__ == "__main__":
   # Server
-  import ctypes
+  import ctypes, sys
   try:
-    remoteobj.CreateServer(ADDR, ctypes).handle_request()
+    remoteobj.CreateServer((sys.argv[1], int(sys.argv[2])), ctypes).handle_request()
   except:
     print 'The remotectypes32 process is angry and quitting.'
     raise
 else:
   # Client
-  import subprocess
-  import time
-  if not PYTHON32:
-    raise Exception('Please set PYTHON32 in remotectypes32.py and pretend that nothing ghetto is happening.')
-  p = subprocess.Popen((PYTHON32, __file__))
-  time.sleep(0.1)
-  ctypes = remoteobj.ConnectProxy(ADDR)
+  import sys, os, time, subprocess, random
+  port = random.randint(10000, 65535)
+
+  if 'PYTHON32' in os.environ:
+    p = subprocess.Popen((os.environ['PYTHON32'], __file__, '127.0.0.1', str(port)))
+  elif sys.platform == 'darwin':
+    p = subprocess.Popen(('/usr/bin/arch', '-i386', '/System/Library/Frameworks/Python.framework/Versions/Current/bin/python2.7', __file__, '127.0.0.1', str(port)))
+  else:
+    raise Exception('Set PYTHON32 in env to an i386 python.')
+
+  for i in range(4):
+    try:
+      time.sleep(i/2.0+0.5)
+      ctypes = remoteobj.ConnectProxy(('127.0.0.1', port))
+      break
+    except socket.error:
+      x,y,z = sys.exc_info()
+  else:
+    raise x,y,z
+
+  # Make `from remotectypes32 import *` work as expected
   __all__ = []
   d = ctypes.__dict__
   for k in d:
