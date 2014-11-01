9bd24613bf6a57d0f96a7d9d18ee130021149dfa
Ned Williamson <nedwilliamson@gmail.com>
2014-11-01 19:48:53 -0400

    libxext dep was missing

--- static2/IDA/ida-deps.sh
+++ static2/IDA/ida-deps.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 sudo apt-get update
-sudo apt-get install libfontconfig1:i386 libXrender1:i386 libsm6:i386 libfreetype6:i386 libglib2.0-0:i386
+sudo apt-get install libfontconfig1:i386 libXrender1:i386 libsm6:i386 libfreetype6:i386 libglib2.0-0:i386 libxext6:i386
