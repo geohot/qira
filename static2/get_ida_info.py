bb73a3cca495516a200749b6d2bb550f2d2adb48
Ned Williamson <nedwilliamson@gmail.com>
2014-11-04 20:36:40 -0500

    new code to work with ida - need linux version for this to be useful

--- static2/get_ida_info.py
+++ static2/get_ida_info.py
@@ -1,22 +1,27 @@
 import idaapi
 import json
 
-# Loop through all the functions
+# all functions from all segments
+all_functions = []
 for ea in Segments():
-    for function_ea in Functions(SegStart(ea), SegEnd(ea)):
-        print json.dumps(x for x in function_ea)
-        #print GetFunctionName(function_ea)
-        #for getting basic blocks when we need them
-        """
-        f = idaapi.FlowChart(idaapi.get_func(function_ea))
-        'endEA', 'id', 'preds', 'startEA', 'succs', 'type'
-        print "preds"
-        for block in f:
-            print dir(block)
-            print "%x - %x [%d]:" % (block.startEA, block.endEA, block.id)
-            for succ_block in block.succs():
-                print "  %x - %x [%d]:" % (succ_block.startEA, succ_block.endEA, succ_block.id)
+    all_functions.append([x for x in Functions(SegStart(ea), SegEnd(ea))])
 
-            for pred_block in block.preds():
-                print "  %x - %x [%d]:" % (pred_block.startEA, pred_block.endEA, pred_block.id)
-        """
+fn_json = json.dumps([item for sublist in all_functions for item in sublist])
+
+with open(idaapi.get_input_file_path()+".ida_info") as f:
+    f.write(fn_json)
+
+#starter code for getting basic blocks when we need them
+"""
+f = idaapi.FlowChart(idaapi.get_func(function_ea))
+'endEA', 'id', 'preds', 'startEA', 'succs', 'type'
+print "preds"
+for block in f:
+print dir(block)
+print "%x - %x [%d]:" % (block.startEA, block.endEA, block.id)
+for succ_block in block.succs():
+    print "  %x - %x [%d]:" % (succ_block.startEA, succ_block.endEA, succ_block.id)
+
+for pred_block in block.preds():
+    print "  %x - %x [%d]:" % (pred_block.startEA, pred_block.endEA, pred_block.id)
+"""
