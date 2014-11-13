f66f4856ae1259da364ebd1e0c5822f55bb9a628
George Hotz <geohot@gmail.com>
2014-11-13 16:21:51 +0000

    qira webstatic

--- middleware/qira_webstatic.py
+++ middleware/qira_webstatic.py
@@ -15,6 +15,10 @@ from qira_base import *
 import json
 import os
 
+def init(lprogram):
+  global program
+  program = lprogram
+
 # *** STATIC CALLS FROM THE FRONTEND ***
 
 @socketio.on('getnames', namespace='/qira')
@@ -45,87 +49,12 @@ def gettagsa():
     ret.append(rret)
   return json.dumps(ret)
 
-@socketio.on('getstaticview', namespace='/qira')
-@socket_method
-def getstaticview(haddr, flat, flatrange):
-  if qira_config.WITH_STATIC == False:
-    return
-  fxn = program.static[fhex(haddr)]['function']
-  if fxn == None or flat == True:
-    addr = fhex(haddr)
-
-    # not a function, return flat view
-    ret = []
-    # find backward
-    i = addr
-    while len(ret) != abs(flatrange[0]):
-      did_append = False
-      # search up to 256 back
-      for j in range(1, 256):
-        if 'len' in program.static[i-j] and program.static[i-j]['len'] == j:
-          i -= j
-          bbb = {'address': ghex(i)}
-          bbb['bytes'] = map(ord, program.static.memory(i, j))
-          ret.append(bbb)
-          did_append = True
-          break
-      if not did_append:
-        i -= 1
-        bbb = {'address': ghex(i)}
-        bbb['bytes'] = map(ord, program.static.memory(i, 1))
-        ret.append(bbb)
-    ret = ret[::-1]
-
-    # find forward
-    i = addr
-    while len(ret) != abs(flatrange[0]) + flatrange[1]:
-      bbb = {'address': ghex(i)}
-      #print program.tags[i]
-      if 'len' in program.static[i]:
-        l = program.static[i]['len']
-        if l == 0:
-          l = 1
-      else:
-        l = 1
-      bbb['bytes'] = map(ord, program.static.memory(i, l))
-      i += l
-      ret.append(bbb)
-
-    for bbb in ret:
-      a = fhex(bbb['address'])
-      bbb['comment'] = program.static[a]['comment']
-      if 'instruction' in program.static[a]:
-        bbb['instruction'] = str(program.static[a]['instruction'])
-      # dests?
-
-    emit('flat', ret)
-  else:
-    blocks = []
-    for b in fxn.blocks:
-      bb = []
-      for i in sorted(b.addresses):
-        bbb = {'address': ghex(i)}
-        bbb['comment'] = program.static[i]['comment']
-        bbb['instruction'] = str(program.static[i]['instruction'])
-        bbb['dests'] = map(lambda (x,y): (ghex(x), y), program.static[i]['instruction'].dests())
-        bb.append(bbb)
-      blocks.append(bb)
-
-    emit('function', {'blocks': blocks})
-
 @socketio.on('gotoname', namespace='/qira')
 @socket_method
 def gotoname(name):
   addr = program.static.get_address_by_name(name)
   if addr != None:
     emit('setiaddr', ghex(addr))
-
-@socketio.on('makefunction', namespace='/qira')
-@socket_method
-def makefunction(iaddr):
-  iaddr = fhex(iaddr)
-  print "*** run analysis at",ghex(iaddr)
-  program.static.analyzer.make_function_at(program.static, iaddr)
 
 @socketio.on('settags', namespace='/qira')
 @socket_method
@@ -148,8 +77,80 @@ def graph_dot():
   #print "DOT RESPONSE", ret
   return ret 
 
-def init(lprogram):
-  global program
-  program = lprogram
+# currently if we aren't using static, we don't want to draw the staticview
+# or be able to makefunction
 
+if qira_config.WITH_STATIC:
+  @socketio.on('getstaticview', namespace='/qira')
+  @socket_method
+  def getstaticview(haddr, flat, flatrange):
+    fxn = program.static[fhex(haddr)]['function']
+    if fxn == None or flat == True:
+      addr = fhex(haddr)
+
+      # not a function, return flat view
+      ret = []
+      # find backward
+      i = addr
+      while len(ret) != abs(flatrange[0]):
+        did_append = False
+        # search up to 256 back
+        for j in range(1, 256):
+          if 'len' in program.static[i-j] and program.static[i-j]['len'] == j:
+            i -= j
+            bbb = {'address': ghex(i)}
+            bbb['bytes'] = map(ord, program.static.memory(i, j))
+            ret.append(bbb)
+            did_append = True
+            break
+        if not did_append:
+          i -= 1
+          bbb = {'address': ghex(i)}
+          bbb['bytes'] = map(ord, program.static.memory(i, 1))
+          ret.append(bbb)
+      ret = ret[::-1]
+
+      # find forward
+      i = addr
+      while len(ret) != abs(flatrange[0]) + flatrange[1]:
+        bbb = {'address': ghex(i)}
+        #print program.tags[i]
+        if 'len' in program.static[i]:
+          l = program.static[i]['len']
+          if l == 0:
+            l = 1
+        else:
+          l = 1
+        bbb['bytes'] = map(ord, program.static.memory(i, l))
+        i += l
+        ret.append(bbb)
+
+      for bbb in ret:
+        a = fhex(bbb['address'])
+        bbb['comment'] = program.static[a]['comment']
+        if 'instruction' in program.static[a]:
+          bbb['instruction'] = str(program.static[a]['instruction'])
+        # dests?
+
+      emit('flat', ret)
+    else:
+      blocks = []
+      for b in fxn.blocks:
+        bb = []
+        for i in sorted(b.addresses):
+          bbb = {'address': ghex(i)}
+          bbb['comment'] = program.static[i]['comment']
+          bbb['instruction'] = str(program.static[i]['instruction'])
+          bbb['dests'] = map(lambda (x,y): (ghex(x), y), program.static[i]['instruction'].dests())
+          bb.append(bbb)
+        blocks.append(bb)
+
+      emit('function', {'blocks': blocks})
+
+  @socketio.on('makefunction', namespace='/qira')
+  @socket_method
+  def makefunction(iaddr):
+    iaddr = fhex(iaddr)
+    print "*** run analysis at",ghex(iaddr)
+    program.static.analyzer.make_function_at(program.static, iaddr)
 
