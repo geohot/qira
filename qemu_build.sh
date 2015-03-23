12e3c9aa3691cddab52002ee63ace574738bbac5
George Hotz <geohot@gmail.com>
2015-03-23 01:22:35 -0700

    add qq to the qemu apt-gets

--- qemu_build.sh
+++ qemu_build.sh
@@ -3,9 +3,9 @@
 # if you don't have ubuntu you are on your own here
 if [ $(which apt-get) ]; then
   echo "fetching qemu build-deps, enter your password"
-  sudo apt-get update
-  sudo apt-get --no-install-recommends -y build-dep qemu
-  sudo apt-get install -y wget flex bison libtool automake autoconf autotools-dev pkg-config libglib2.0-dev
+  sudo apt-get update -qq
+  sudo apt-get --no-install-recommends -qq -y build-dep qemu
+  sudo apt-get install -qq -y wget flex bison libtool automake autoconf autotools-dev pkg-config libglib2.0-dev
 else
   echo "WARNING: you don't have apt-get, you are required to fetch the build deps of QEMU on your own"
 fi
