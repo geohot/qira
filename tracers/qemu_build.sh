c593de81f58581bc2347eaf44c13f76a94d4fa5e
Dominic Chen <d.c.ddcc@gmail.com>
2015-03-23 15:55:03 +0000

    use patch for qemu instead of pre-modified files

--- tracers/qemu_build.sh
+++ tracers/qemu_build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+QEMU_VERSION=2.1.0-rc0
+
 # if you don't have ubuntu you are on your own here
 if [ $(which apt-get) ]; then
   echo "fetching qemu build-deps, enter your password"
@@ -18,9 +20,9 @@ if [ ! -d qemu/qemu-latest ]; then
   rm -rf qemu
   mkdir -p qemu
   cd qemu
-  wget http://wiki.qemu-project.org/download/qemu-2.1.0-rc0.tar.bz2
-  tar xf qemu-2.1.0-rc0.tar.bz2
-  ln -s qemu-2.1.0-rc0 qemu-latest
+  wget http://wiki.qemu-project.org/download/qemu-$QEMU_VERSION.tar.bz2
+  tar xf qemu-$QEMU_VERSION.tar.bz2
+  ln -s qemu-$QEMU_VERSION qemu-latest
 
   ln -s qemu-latest/arm-linux-user/qemu-arm qira-arm
   ln -s qemu-latest/i386-linux-user/qemu-i386 qira-i386
@@ -30,22 +32,11 @@ if [ ! -d qemu/qemu-latest ]; then
   ln -s qemu-latest/mips-linux-user/qemu-mips qira-mips
 
   cd qemu-latest
-  mv tci.c tci.c.bak
-  mv disas.c disas.c.bak
-  mv linux-user/qemu.h linux-user/qemu.h.bak
-  mv linux-user/main.c linux-user/main.c.bak
-  mv linux-user/strace.c linux-user/strace.c.bak
-  mv linux-user/strace.list linux-user/strace.list.bak
-  cd ../../
+  patch -p1 < ../../qemu.patch
+  cd ../..
 fi
 
 cd qemu/qemu-latest
-ln -sf ../../qemu_mods/tci.c tci.c
-ln -sf ../../qemu_mods/disas.c disas.c
-ln -sf ../../../qemu_mods/qemu.h linux-user/qemu.h
-ln -sf ../../../qemu_mods/main.c linux-user/main.c
-ln -sf ../../../qemu_mods/strace.c linux-user/strace.c
-ln -sf ../../../qemu_mods/strace.list linux-user/strace.list
 ./configure --target-list=i386-linux-user,x86_64-linux-user,arm-linux-user,ppc-linux-user,aarch64-linux-user,mips-linux-user --enable-tcg-interpreter --enable-debug-tcg --cpu=unknown --enable-tcg-interpreter --enable-debug-tcg --cpu=unknown
 make -j $(grep processor < /proc/cpuinfo | wc -l)
 
