bb1c57746bc8714b8660e7845c8df1616402b92c
Ned Williamson <nedwilliamson@gmail.com>
2015-08-11 13:41:48 -0400

    loader fails nicely on non-ELF inputs

--- static2/builtin/loader.py
+++ static2/builtin/loader.py
@@ -1,6 +1,7 @@
 from elftools.elf.elffile import ELFFile
 from elftools.elf.sections import SymbolTableSection
 from elftools.elf.relocation import RelocationSection
+from elftools.common.exceptions import ELFError
 import struct
 
 def get_arch(fb):
@@ -19,7 +20,11 @@ def get_arch(fb):
 
 
 def load_binary(static):
-  elf = ELFFile(open(static.path))
+  try:
+    elf = ELFFile(open(static.path))
+  except ELFError:
+    print "*** loader error: non-ELF detected"
+    return
 
   # TODO: replace with elf['e_machine']
   progdat = open(static.path).read(0x20)
