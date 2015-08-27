5434732b63a2ac775af9275b812a886ef64f2d9e
George Hotz <geohot@gmail.com>
2015-08-27 07:17:06 +0000

    update run_tests_static

--- run_tests_static.sh
+++ run_tests_static.sh
@@ -5,8 +5,9 @@ source venv/bin/activate
 # ned's tests, which are really awesome
 # why weren't these used?
 cd tests_auto
-./autogen-extras.sh
-python autogen.py --dwarf --all
+#./autogen-extras.sh
+#python autogen.py --dwarf --all
+python autogen.py --dwarf
 cd ../
 cd static2
 python testing.py
