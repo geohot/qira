9052a9da806fd0b588bafc0255c9e519642be853
Yahya Lmallas <merruk.company@gmail.com>
2016-02-06 03:46:38 +0100

    FIX: switch to distutils and fix cpp warnings, eliminate '-Wsign-compare' problems from Trace.cpp

--- qiradb/setup.py
+++ qiradb/setup.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python2.7
+import os
 from version import __version__
-from setuptools import setup, Extension, Command
+from distutils.core import setup, Extension
+import distutils.sysconfig
+
+cfg_vars = distutils.sysconfig.get_config_vars()
+for key, value in cfg_vars.items():
+    if type(value) == str:
+        cfg_vars[key] = value.replace("-Wstrict-prototypes", "")
 
 # the c++ extension module
+os.environ["CC"] = "g++"
+os.environ["CXX"] = "g++"
 extension_mod = Extension("qiradb._qiradb", sources=["qiradb/Trace.cpp", "qiradb/_qiradb.cpp"], language="c++")
 
 url="https://github.com/BinaryAnalysisPlatform"
