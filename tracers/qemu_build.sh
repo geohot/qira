7fd8778155d48c956b7f273cb54af5031cd3489b
George Hotz <george@comma.ai>
2019-03-24 14:33:33 -0700

    make -j is fine

--- tracers/qemu_build.sh
+++ tracers/qemu_build.sh
@@ -8,5 +8,5 @@ fi
 
 cd qemu/qemu
 ./configure --target-list=i386-linux-user,x86_64-linux-user,arm-linux-user,ppc-linux-user,aarch64-linux-user,mips-linux-user,mipsel-linux-user --enable-tcg-interpreter --enable-debug-tcg --cpu=unknown --python=python
-make -j $(grep processor < /proc/cpuinfo | wc -l)
+make -j
 
