40ff873194c3a8fc9ed0960b2296a43fed1e095d
Charles Ellis <hamled@hamled.name>
2019-03-25 13:14:16 -0700

    QEMU tracer uses version 3.1.0
    QEMU now uses Capstone for its disassembly output, so that dependency
    has been added as well.

--- tracers/qemu_build.sh
+++ tracers/qemu_build.sh
@@ -2,10 +2,10 @@
 
 if [ ! -d qemu/qemu ]; then
   cd qemu
-  git clone https://github.com/geohot/qemu.git --depth 1 --branch qira
+  git clone https://github.com/geohot/qemu.git --depth 1 --branch v3.1.0-qira
   cd ..
 fi
 
 cd qemu/qemu
-./configure --target-list=i386-linux-user,x86_64-linux-user,arm-linux-user,ppc-linux-user,aarch64-linux-user,mips-linux-user,mipsel-linux-user --enable-tcg-interpreter --enable-debug-tcg --cpu=unknown --python=python
+./configure --target-list=i386-linux-user,x86_64-linux-user,arm-linux-user,ppc-linux-user,aarch64-linux-user,mips-linux-user,mipsel-linux-user --enable-tcg-interpreter --enable-debug-tcg --enable-capstone --cpu=unknown --python=python
 make -j$(getconf _NPROCESSORS_ONLN) 
