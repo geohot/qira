61dff95b2ac94ec92d5c95b2b167743ed41eb530
George Hotz <geohot@gmail.com>
2019-04-15 22:02:51 -0700

    Revert "rip out broken CDA"
    This reverts commit 950e97b21300036eba54df02d2c6e7b0d4c991e9.

---
+++ cda/cda_config.py
@@ -0,0 +1,7 @@
+#ci.Config.set_library_file(basedir+"/clang/build/Release+Asserts/lib/libclang.so")
+#ci.Config.set_library_file(basedir+"/libclang.so")
+LIBCLANG_PATH = '/usr/lib/x86_64-linux-gnu/libclang.so.1'
+#args.append(basedir+"/clang-latest/build/Release+Asserts/lib/clang/3.4.2/include")
+#args.append(basedir+"/include")
+CLANG_INCLUDES = '/usr/lib/llvm-3.4/lib/clang/3.4/include'
+
